7e19dd23bcb426a8b8691f6bc43398fa7b62326e
Rolando <rolando_osuna@live.com.mx>
2021-10-07 15:38:18 -0600

    Actualizacion de la tabla

--- NavBar.php
+++ NavBar.php
@@ -1,12 +1,14 @@
 <link rel="stylesheet" href="estilos.css">
-<nav class="row margen py-2 bg-navbar align-items-center">
-    <div class="col-8">
-        <a class="text-decoration-none link-light justify-content-end" href="index.php">
-            <img class="img-fluid justify-content-start ps-3" src="/IMG/Raptor-Navbar.png" width="10%" alt="">
-        </a>
+<nav class="row margen py-2 bg-navbar ">
+    <div class="row justify-content-end align-items-center nav">
+        <div class="col-8">
+            <a class="text-decoration-none link-light justify-content-end" href="index.php">
+                <img class="img-fluid justify-content-start ps-3" src="/IMG/Raptor-Navbar.png" width="10%" alt="">
+            </a>
+        </div>
+        <div class="col-1 "><a class="text-decoration-none link-light " href="index.php">Inicio</a></div>
+        <div class="col-1 "><a class="text-decoration-none link-light " href="">Nosotros</a></div>
+        <div class="col-1 "><a class="text-decoration-none link-light " href="">Servicios</a></div>
+        <div class="col-1 "><a class="text-decoration-none link-light " href="">Contacto</a></div>
     </div>
-        <div class="col-1 "><a class="text-decoration-none link-light justify-content-end" href="index.php">Inicio</a></div>
-        <div class="col-1 "><a class="text-decoration-none link-light justify-content-end" href="">Nosotros</a></div>
-        <div class="col-1 "><a class="text-decoration-none link-light justify-content-end" href="">Servicios</a></div>
-        <div class="col-1 "><a class="text-decoration-none link-light justify-content-end" href="">Contacto</a></div>
 </nav>
--- index.php
+++ index.php
@@ -214,15 +214,23 @@
             </div>
         </div>
 <!-- TABLA -->
-        <div class="row align-items-center">
-            <div class="col-12">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-12 ">
                 <div class="table-responsive text-center">
                     <table class="table table-striped table-hover table-sm border-bordered">
                         <thead>
                             <tr class="encabezado-tabla text-white fs-md-5" >
                                 <th class="fw-normal">
                                     <p class="">
-                                        <span class="text-info ">Emprendedor</span><br>
+                                        <span class="text-info ">Jugador Casual</span><br>
+                                        <span class="text-white ">$299.00</span><br> 
+                                        <span class="text-white-50">MXM al MES</span><br>
+                                        <span class="text-muted">(pago mensual)</span>
+                                    </p>
+                                </th>
+                                <th class="fw-normal">
+                                    <p class="">
+                                        <span class="text-info ">Aficionado intermedio</span><br>
                                         <span class="text-white ">$699.00</span><br> 
                                         <span class="text-white-50">MXM al MES</span><br>
                                         <span class="text-muted">(pago mensual)</span>
@@ -230,81 +238,87 @@
                                 </th>
                                 <th class="fw-normal">
                                     <p class="">
-                                        <span class="text-info ">Negocios</span><br>
-                                        <span class="text-white ">$999.00</span><br> 
+                                        <span class="text-info ">Aficionado avanzado</span><br>
+                                        <span class="text-white ">$1,299.00</span><br> 
                                         <span class="text-white-50">MXM al MES</span><br>
                                         <span class="text-muted">(pago mensual)</span>
                                     </p>
                                 </th>
-                                <th class="fw-normal">
-                                    <p class="">
-                                        <span class="text-info ">Profesional</span><br>
-                                        <span class="text-white ">$1,999.00</span><br> 
-                                        <span class="text-white-50">MXM al MES</span><br>
-                                        <span class="text-muted">(pago mensual)</span>
-                                    </p>
-                                </th>
-                                <th class=" pb-5 pb-md-0">
-                                    <p>
-                                        <img  src="/IMG/Raptor-Navbar.png" width="20%" alt="">
-                                    </p>
+                                <th class="pb-5 pb-sm-4 pb-lg-4 ">
+                                    <img src="/IMG/Raptor-Navbar.png" width="80px" alt="">
                                 </th>
                             </tr>
                         </thead>
+
                         <tbody>
-                            <tr> 
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td class="arial pt-3 pt-2">Usuarios Ilimitados</td>
+                            <tr>
+                                <td class="p-3"><img class="" src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class="p-3"><img class="" src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class="p-3"><img class="" src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class=" "><img src="/IMG/people-fill.svg"  width="50px" alt="Usuarios Ilimitados"><!--Usuarios Ilimitados--></td>
                             </tr>
                             <tr>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td class="arial pt-3">Gestor Inmobiliario</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td class="arial pt-3">Gestor de Prospectos</td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class=" "><img src="/IMG/house-door-fill.svg"  width="50px" alt=""><!--Gestor Inmobiliario--></td>
                             </tr>
                             <tr>
                                 <td></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td class="arial pt-3">Gestor de Estatus</td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class=" "><img src="/IMG/card-text.svg" width="50px" alt=""><!--Gestor de Prospectos--></td>
                             </tr>
                             <tr>
                                 <td></td>
-                                <td></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td class="arial pt-3">Gestor de Contratos</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td class="arial pt-3">Gestor de Proyectos</td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class=" "><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-tools" width="50px" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                        <path d="M3 21h4l13 -13a1.5 1.5 0 0 0 -4 -4l-13 13v4" />
+                                        <line x1="14.5" y1="5.5" x2="18.5" y2="9.5" />
+                                        <polyline points="12 8 7 3 3 7 8 12" />
+                                        <line x1="7" y1="8" x2="5.5" y2="9.5" />
+                                        <polyline points="16 12 21 17 17 21 12 16" />
+                                        <line x1="16" y1="17" x2="14.5" y2="18.5" />
+                                    </svg><!--Gestor de Estatus--></td>
                             </tr>
                             <tr>
                                 <td></td>
                                 <td></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td class="arial pt-3">Gestor de Correos</td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class=" "><img src="/IMG/1976101.svg" width="55px" alt=""><!--Gestor de Contratos--></td>
                             </tr>
                             <tr>
                                 <td></td>
                                 <td></td>
-                                <td><img class="py-2" src="/IMG/check2-circle.svg" width="15%" alt=""></td>
-                                <td class="arial pt-3">Futuras Aduciones </td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class=" "><img src="/IMG/journal-text.svg" width="50px" alt=""><!--Gestor de Proyectos--></td>
                             </tr>
                             <tr>
-                                <td class="fst-italic pt-2">8 GB</td>
-                                <td class="fst-italic pt-2">16 GB</td>
-                                <td class="fst-italic pt-2">32 GB</td>
-                                <td class="arial pt-2">Hosting</td>
+                                <td></td>
+                                <td></td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class=" "><img src="/IMG/envelope-fill.svg" width="50px" alt=""><!--Gestor de Correos--></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td class="p-3"><img src="/IMG/check2-circle.svg" width="25px" alt=""></td>
+                                <td class=" ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-database-export" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                        <ellipse cx="12" cy="6" rx="8" ry="3" />
+                                        <path d="M4 6v6c0 1.657 3.582 3 8 3a19.84 19.84 0 0 0 3.302 -.267m4.698 -2.733v-6" />
+                                        <path d="M4 12v6c0 1.599 3.335 2.905 7.538 2.995m8.462 -6.995v-2m-6 7h7m-3 -3l3 3l-3 3" />
+                                    </svg><!--Futuras Aduciones-->
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="fst-italic p-3">8 GB</td>
+                                <td class="fst-italic p-3">16 GB</td>
+                                <td class="fst-italic p-3">32 GB</td>
+                                <td class=" "><img src="/IMG/cloud-plus-fill.svg" width="50px" alt=""><!--Hosting--></td>
                             </tr>
                         </tbody>
 
