40d0c489cd2598bad450f5ef4ee8accd248634e7
Rolando <rolando_osuna@live.com.mx>
2021-10-13 14:11:43 -0600

    Ajuste Descripcion y NavBar terminado

--- Descripcion.php
+++ Descripcion.php
@@ -13,7 +13,7 @@
 <?php require('NavBar.php') ?>
     <header class="container">
         <div class="row bg-light p-3 my-3 shadow rounded">
-            <div class="col-12 col-lg-8 ps-5">
+            <div class="col-12 col-md-6 col-lg-7 ps-0 ps-lg-5">
                 <!-- CARRUSEL -->
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-indicators">
@@ -44,36 +44,36 @@
                             aria-label="Slide 4">
                         </button>
                     </div>
-                    <div class="carousel-inner ps-5">
+                    <div class="carousel-inner ps-0 ps-lg-5">
                         <div class="carousel-item active">
-                            <img src="/IMG/Abarrotes/Carrusel-Descripcion1.jpg" class="d-block" width="80%" loading="lazy">
+                            <img src="/IMG/Abarrotes/Carrusel-Descripcion1.jpg" class="img-fluid d-block" width="400px" loading="lazy">
                         </div>
                         <div class="carousel-item">
-                            <img src="/IMG/Abarrotes//Carrusel-Descripcion2.jpg" class="d-block" width="80%" alt="...">
+                            <img src="/IMG/Abarrotes//Carrusel-Descripcion2.jpg" class="img-fluid d-block" width="400px" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="/IMG/Abarrotes/Carrusel-Descripcion3.jpg" class="d-block" width="80%" alt="...">
+                            <img src="/IMG/Abarrotes/Carrusel-Descripcion3.jpg" class="img-fluid d-block" width="400px" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="/IMG/Abarrotes/Carrusel-Descripcion4.jpg" class="d-block" width="80%" alt="...">
+                            <img src="/IMG/Abarrotes/Carrusel-Descripcion4.jpg" class="img-fluid d-block" width="400px" alt="...">
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <button class="carousel-control-prev pe-5" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Anterior</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <button class="carousel-control-next ps-5" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Siguiente</span>
                     </button>
                 </div>
             </div>
-            <div class="col-12 col-lg-4 align-self-center">
-                <p class="display-5 titulo text-center">Abarrotes</p>
+            <div class="col-12 col-md-6 col-lg-5 align-self-center justify-content-center ">
+                <p class="display-5 titulo text-center pt-3 pt-lg-0">Abarrotes</p>
                 <hr>
-                <p class="fs-5 texto lh-5 pt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, iure pariatur. Ea accusantium nostrum ipsam neque omnis nemo repellendus, velit perferendis debitis, alias dolor quia aspernatur recusandae earum iure. Fugiat temporibus, minima corporis optio harum ad, rem, nemo ullam quae aspernatur repellat dolorum non vel.</p>
-                <p class="text-end text-success fs-4 pt-3">$500.00</p>
-                <div class="col-12 d-grid pt-3 mx-auto">
+                <p class="fs-5 texto lh-5 pt-lg-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, iure pariatur. Ea accusantium nostrum ipsam neque omnis nemo repellendus, velit perferendis debitis, alias dolor quia aspernatur recusandae earum iure. Fugiat temporibus, minima corporis optio harum ad, rem, nemo ullam quae aspernatur repellat dolorum non vel.</p>
+                <p class="text-end text-success fs-4 pt-0 pt-lg-3">$500.00</p>
+                <div class="col-12 d-grid pt-0 pt-lg-3 mx-auto">
                     <button class="btn btn-danger" type="button">Boton</button>    
                 </div>
             </div>
--- NavBar.php
+++ NavBar.php
@@ -1,14 +1,47 @@
 <link rel="stylesheet" href="estilos.css">
-<nav class="row margen py-2 bg-navbar navbar">
+<nav class="row margen py-2 bg-navbar navbar d-none d-lg-block">
     <div class="row justify-content-end align-items-center ">
         <div class="col-8">
             <a class="text-decoration-none link-light justify-content-end ps-3" href="index.php">
-                <img class="img-fluid justify-content-start " src="/IMG/Raptor-Navbar.png" width="75px" alt="">
+                <img class="img-fluid justify-content-start " src="/IMG/Raptor-Navbar2.png" width="175px" alt="">
             </a>
         </div>
-        <div class="col-1 fs-5"><a class="text-decoration-none link-light " href="index.php">Inicio</a></div>
-        <div class="col-1 fs-5"><a class="text-decoration-none link-light " href="">Nosotros</a></div>
-        <div class="col-1 fs-5"><a class="text-decoration-none link-light " href="Servicios.php">Servicios</a></div>
-        <div class="col-1 fs-5"><a class="text-decoration-none link-light " href="">Contacto</a></div>
+        <div class="col-4 nav justify-content-around">
+            <div class="col-1 fs-5"><a class="text-decoration-none link-light " href="index.php">Inicio</a></div>
+            <div class="col-1 fs-5"><a class="text-decoration-none link-light " href="">Nosotros</a></div>
+            <div class="col-1 fs-5"><a class="text-decoration-none link-light " href="Servicios.php">Servicios</a></div>
+            <div class="col-1 fs-5"><a class="text-decoration-none link-light " href="">Contacto</a></div>
+        </div>
+    </div>
+</nav>
+<nav class="navbar navbar-expanded-md bg-navbar navbar-light text-end d-block d-lg-none">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"><img class="img-fluid" src="/IMG/Raptor-Navbar.png" width="75px"alt=""></a>
+        <button
+            class="navbar-toggler bg-light"
+            type="button"
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse text-end fs-5 align-items-center" id="navbarNav">
+            <ul class="navbar-nav pt-2 pt-md-0">
+                <li class="nav-item pe-md-3">
+                    <a class="text-decoration-none link-light" aria-current="page" href="index.php">Inicio</a>
+                </li>
+                <li class="nav-item pe-md-3">
+                    <a class="text-decoration-none link-light" aria-current="page" href="Nosotros.php">Nosotros</a>
+                </li>
+                <li class="nav-item pe-md-3">
+                    <a class="text-decoration-none link-light" aria-current="page" href="Servicios.php">Servicios</a>
+                </li>
+                <li class="nav-item pe-md-3">
+                    <a class="text-decoration-none link-light" aria-current="page" href="Contacto.php">Contacto</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
